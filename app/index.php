0b5c1b24158e5347e875e2fde81ad7feeda35875
Ibrahim I Musabah <i.musabah92@gmail.com>
2020-03-08 14:10:48 +0200

    save

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo "Our App"?></title>
+</head>
+
+<body>
+<?php
+    echo "Welcome at Our Server......";
+?>
+</body>
+
+</html>
